6cc4b4a4efdf8d1468e5247d04a834b58b8fc5d8
a8520238 <360238076@qq.com>
2020-04-03 14:57:28 +0800

    剑指offer第二题 stringbuffer

---
+++ 剑指offer/replaceSpace_2.java
@@ -0,0 +1,21 @@
+/**
+ * 请实现一个函数，将一个字符串中的每个空格替换成“%20”。
+ * 例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy
+ */
+
+package 剑指offer;
+//Stringbuffer
+public class replaceSpace_2 {
+    public String replaceSpace(StringBuffer str) {
+    	StringBuffer buffer = new StringBuffer();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                buffer.append("%20");
+            }
+            else {
+                buffer.append(str.charAt(i));
+            }
+        }
+        return buffer.toString();
+    }
+}
