c958aa03c1ef572988c4926ed3a1e65b7fd32b0d
a8520238 <360238076@qq.com>
2020-04-05 10:29:46 +0800

    剑指offer第九题 变态跳台阶 数学

---
+++ 剑指offer/JumpFloorII_9.java
@@ -0,0 +1,14 @@
+/**一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。 */
+package 剑指offer;
+
+//数学 fn = 2*(f(n-1))
+public class JumpFloorII_9 {
+    public int JumpFloorII(int target) {
+        int res = 1;
+        while (target > 1) {
+            res = res * 2;
+            target--;
+        }
+        return res;
+    }
+}
