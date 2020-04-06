66fe0afd6b1c131fe2e833ef83b162829c383a3b
a8520238 <360238076@qq.com>
2020-04-06 11:00:08 +0800

    剑指offer第七题斐波那契数列

--- offer/Fibonacci_7.java
+++ offer/Fibonacci_7.java
@@ -1,6 +1,6 @@
 /**大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0）。 
 n<=39  */
-package 剑指offer;
+package offer;
 public class Fibonacci_7 {
     public int Fibonacci(int n) {
         if (n < 1) {
