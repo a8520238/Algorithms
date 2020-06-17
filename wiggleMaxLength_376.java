c4b047fba623ecaf1fc460f148e7f61448e1443f
a8520238 <360238076@qq.com>
2020-06-17 14:28:49 +0800

    leetcode 376中等 摆动序列 dp

---
+++ wiggleMaxLength_376.java
@@ -0,0 +1,72 @@
+/**
+ * 如果连续数字之间的差严格地在正数和负数之间交替，则数字序列称为摆动序列。第一个差（如果存在的话）可能是正数或负数。少于两个元素的序列也是摆动序列。
+
+例如， [1,7,4,9,2,5] 是一个摆动序列，因为差值 (6,-3,5,-7,3) 是正负交替出现的。相反, [1,4,7,2,5] 和 [1,7,4,5,5] 不是摆动序列，第一个序列是因为它的前两个差值都是正数，第二个序列是因为它的最后一个差值为零。
+
+给定一个整数序列，返回作为摆动序列的最长子序列的长度。 通过从原始序列中删除一些（也可以不删除）元素来获得子序列，剩下的元素保持其原始顺序。
+
+示例 1:
+
+输入: [1,7,4,9,2,5]
+输出: 6 
+解释: 整个序列均为摆动序列。
+
+ */
+
+class wiggleMaxLength_376 {
+    public int wiggleMaxLength(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int len = nums.length;
+        boolean f = false;
+        for (int n : nums) {
+            if (n == 0) {
+                f = true;
+                len--;
+            }
+        }
+        if (len < 2) {
+            return f? len + 1: len;
+        }
+
+        int[] add = new int[nums.length - 1];
+        int i = 0;
+        for (i = 0; i < add.length; i++) {
+            add[i] = nums[i+1] - nums[i];
+            if (add[i] == 0) {
+                continue;
+            } else {
+                break;
+            }
+        }
+
+        int count = 2;
+
+        int pre = add[i] > 0? 1: -1;
+
+        for (; i < add.length; i++) {
+            add[i] = nums[i+1] - nums[i];
+            if (add[i]*pre < 0) {
+                pre = -pre;
+                count++;
+            } else {
+                continue;
+            }
+        }
+        return count;
+    }
+}
+//dp方法
+/*
+public int wiggleMaxLength(int[] nums) {
+    int down = 1, up = 1;
+    for (int i = 1; i < nums.length; i++) {
+        if (nums[i] > nums[i - 1])
+            up = down + 1;
+        else if (nums[i] < nums[i - 1])
+            down = up + 1;
+    }
+    return nums.length == 0 ? 0 : Math.max(down, up);
+}
+*/
